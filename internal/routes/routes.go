d5d7007aca73f8f6453dfaf4513d2c5450a1f7d6
JLarky <jlarky@gmail.com>
2023-08-26 11:48:18 -0600

    custom handler

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,11 +1,22 @@
 package routes
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type Handler func(w http.ResponseWriter, r *http.Request) error
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := h(w, r); err != nil {
+		// handle returned error here.
+		w.WriteHeader(503)
+		w.Write([]byte("bad"))
+	}
+}
 
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
@@ -13,9 +24,21 @@ func NewRouter() *chi.Mux {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world 1123"))
 	})
+	r.Method("GET", "/", Handler(customHandler))
 
 	return r
+}
+
+func customHandler(w http.ResponseWriter, r *http.Request) error {
+	q := r.URL.Query().Get("err")
+
+	if q != "" {
+		return errors.New(q)
+	}
+
+	w.Write([]byte("foo bar"))
+	return nil
 }
